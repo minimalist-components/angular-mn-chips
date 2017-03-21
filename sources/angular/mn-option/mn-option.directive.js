6b5e7977db0d21f44968dce823e6fad630b1bce2
Darlan Mendonça <darlanmendonca@gmail.com>
2017-03-21 16:01:20 -0300

    begin directive

--- sources/angular/mn-option/mn-option.directive.js
+++ sources/angular/mn-option/mn-option.directive.js
@@ -5,11 +5,40 @@ angular
 function mnChipsDirective($parse, $timeout) {
   return {
     restrict: 'E',
+    controller,
     link,
     require: 'ngModel',
   }
 
-  function link() {
+  function controller($scope, $element, $attrs) {
+    const name = $element.attr('name')
+      || $attrs.ngModel.split('.')[$attrs.ngModel.split('.').length - 1]
 
+    $element.attr('name', name)
+    $element.find('input').attr('name', name)
+  }
+
+  function link(scope, element, attributes) {
+    const modelValue = $parse(attributes.ngModel)(scope)
+    const modelApplied = angular.equals(element[0].value, modelValue)
+
+    if (!modelApplied) {
+      $timeout(() => {
+        element[0].value = modelValue
+        $parse(attributes.ngModel).assign(scope, element[0].value)
+      }, 0)
+    }
+
+    element.on('change', () => {
+      $timeout(() => {
+        $parse(attributes.ngModel).assign(scope, element[0].value)
+        scope.$apply()
+      }, 0)
+    })
+
+    // const dirtyLabel = element[0].querySelector('label + label')
+    // if (dirtyLabel) {
+    //   element[0].removeChild(dirtyLabel)
+    // }
   }
 }
